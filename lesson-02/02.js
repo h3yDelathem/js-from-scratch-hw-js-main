0cb42c1117957ced95e5de9d4377b9d1023a3339
Alauch <h3ydelathem@gmail.com>
2025-09-21 23:51:57 +0300

    second page three

--- lesson-02/02.js
+++ lesson-02/02.js
@@ -7,14 +7,14 @@
 * Для успешного прохождения тестов не меняйте названия переменных!
 */
 
-let dice1 = Math.floor(Math.random()*6)  // your code
-let dice2 = Math.floor(Math.random()*6) // your code
-let isWinningDouble = false // your code
-if (isWinningDouble = dice1 === dice2 && dice1 > 3) {
-    isWinningDouble = true
-    console.log('Выигрышный дубль!')
-} else {
-console.log('Не выиграшный дубль.') }
+// let dice1 = Math.floor(Math.random()*6)  // your code
+// let dice2 = Math.floor(Math.random()*6) // your code
+// let isWinningDouble = false // your code
+// if (isWinningDouble = dice1 === dice2 && dice1 > 3) {
+//     isWinningDouble = true
+//     console.log('Выигрышный дубль!')
+// } else {
+// console.log('Не выиграшный дубль.') }
 /*
 console.log('Первый бросок: ' + dice1)
 console.log('Второй бросок: ' + dice2)
--- lesson-02/03.js
+++ lesson-02/03.js
@@ -5,5 +5,17 @@
  */
 
 const score = 70 // тестовое значение, можно изменять
-let grade
+let grade = ''
+if (score >= 90 && score <= 100) {
+    grade = 'A';
+} else if (score >= 80 && score <= 89) {
+    grade = 'B';
+} else if (score >= 70 && score <= 79) {
+    grade = 'C';
+} else if (score >= 50 && score <= 69) {
+    grade = 'D';
+} else {
+    grade = 'F';
+};
+console.log(grade);
 // your code
