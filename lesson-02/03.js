263c30f1ce462fd6cc15f4f9d40ad1b9131aaf24
Alauch <h3ydelathem@gmail.com>
2025-09-25 20:33:47 +0300

    three lesson one

--- lesson-02/03.js
+++ lesson-02/03.js
@@ -4,18 +4,18 @@
  * Результат сохраните в переменной grade.
  */
 
-const score = 70 // тестовое значение, можно изменять
-let grade = ''
-if (score >= 90 && score <= 100) {
-    grade = 'A';
-} else if (score >= 80 && score <= 89) {
-    grade = 'B';
-} else if (score >= 70 && score <= 79) {
-    grade = 'C';
-} else if (score >= 50 && score <= 69) {
-    grade = 'D';
-} else {
-    grade = 'F';
-};
-console.log(grade);
+// const score = 70 // тестовое значение, можно изменять
+// let grade = ''
+// if (score >= 90 && score <= 100) {
+//     grade = 'A';
+// } else if (score >= 80 && score <= 89) {
+//     grade = 'B';
+// } else if (score >= 70 && score <= 79) {
+//     grade = 'C';
+// } else if (score >= 50 && score <= 69) {
+//     grade = 'D';
+// } else {
+//     grade = 'F';
+// };
+// console.log(grade);
 // your code
--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,3 +1,6 @@
 // Напишите функцию calculateAverage, которая принимает 2 числа и возвращает их среднее арифметическое
-
-function calculateAverage() {}
+function calculateAverage(a, b) {
+    calculateAverage =(a+b)/2
+}
+calculateAverage(3, 3)
+console.log(calculateAverage)
