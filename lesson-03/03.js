3dc3ee4211994fde9fba9ca7edbe03e0cfde0d16
Alauch <h3ydelathem@gmail.com>
2025-10-04 22:00:32 +0300

    four lesson one

--- lesson-03/03.js
+++ lesson-03/03.js
@@ -1,13 +1,13 @@
 // Разработайте функцию findLargest, которая принимает три числа и возвращает наибольшее из них.
 
-function findLargest(a, b, c) {
-    if (a > c && a > b){
-        return a;
-    } else if (b > a && b > c){
-        return b;
-    } else {
-        return c;
-    }
-}
-
-console.log(findLargest(5113, 231 , 122));
+// function findLargest(a, b, c) {
+//     if (a > c && a > b){
+//         return a;
+//     } else if (b > a && b > c){
+//         return b;
+//     } else {
+//         return c;
+//     }
+// }
+//
+// console.log(findLargest(5113, 231 , 122));
--- lesson-04/01.js
+++ lesson-04/01.js
@@ -12,5 +12,13 @@
 Пример использования:
 includesElement([1, 2, 3], 2)  // должен вернуть `true`.
 */
+function includesElement(array, element) {
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === element) {
+            return true
+        }
+    }
+            return false
+    }
 
-function includesElement() {}
+console.log(includesElement([1, 2, 3], 2 ));
