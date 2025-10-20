4852e9be2620f39d3190722e0dafa6d375261645
Alauch <h3ydelathem@gmail.com>
2025-10-20 19:37:46 +0300

    five lesson two

--- lesson-05/02.js
+++ lesson-05/02.js
@@ -35,4 +35,10 @@ const gallery = {
   'The Scream': 'Edvard Munch',
 }
 
-function updateGallery() {}
+function updateGallery(gallery, title, author) {
+    gallery[title] = author;
+}
+
+updateGallery(gallery, 'Mona Lisa', 'Leonardo da Vinci, 1503-1506')
+updateGallery(gallery, 'The Persistence of Memory', 'Salvador Dali')
+
--- lesson-06/02.js
+++ lesson-06/02.js
@@ -24,12 +24,12 @@ const doubledNumbers = map(numbers, (element, index) => {
 console.log(doubledNumbers) // Должен вывести: [2, 4, 6, 8, 10]
 */
 
-const map = (array, callback) => {
-    const result = [];
-
-    for (let i = 0; i < array.length; i++) {
-        const mappedValue = callback(array[i], i);
-        result.push(mappedValue);
-    }
-    return result;
-}
+// const map = (array, callback) => {
+//     const result = [];
+//
+//     for (let i = 0; i < array.length; i++) {
+//         const mappedValue = callback(array[i], i);
+//         result.push(mappedValue);
+//     }
+//     return result;
+// }
